a966d4ef581441e28e81146d42f3824acf8c5514
Yorick Flannagan <yorick.flannagan@gmail.com>
2017-05-07 18:00:26 -0300

    Building scripts

--- linux/config.sh
+++ linux/config.sh
@@ -1,27 +1,56 @@
 #!/bin/bash
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Nharu Libraries build facility
+# Copyleft (c) 2015 by The Crypthing Initiative
+#
+# Warnings:
+# For debuggin installation, set CFLAGS environment variable with at least -D_DEBUG_
+# For struct alignment, set CFLAGS environment variable with at least -D_ALIGN_
+#
+# Variables exported to make files:
+#	BUILD_TARGET: Installation directory
+#	OPENSSL_INCLUDE: OpenSSL headers
+#	OPENSSL_LIB: OpenSSL libraries (required if --enable-shared)
+#	IDN_INCLUDE: GNU IDN headers
+#	IDN_LIB: GNU IDN library (required if --enable-shared)
+#	JAVA_INCLUDE: JDK base include header (required if --enable-java or --enable-shared)
+#	JAVA_PLATFORM: JDK system dependent include header (required if --enable-java or --enable-shared)
+#	DLA_LIB: Linux libdl.a (required if --enable-shared)
+#	ANT_HOME: Apache Ant root directory (required if --enable-java)
+#	ANT_CONTRIB: Ant Contrib library (required if --enable-java)
+#	CC: ANSI C Compiler
+#	AR: Archiver
+#	CFLAGS: Compiler flags
+#	ARFLAGS: Archiver flags
+#	LDFLAGS: Linker flags
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# TODO
+#	Project Makefile
+#	install target
+#	@if [ -d "$(BUILD_TARGET)/include/libnharu" ]; then rm -f -R "$(BUILD_TARGET)/include/libnharu"; fi; \
+#	if [ -d "$(BUILD_TARGET)/include/pkcs11" ]; then rm -f -R "$(BUILD_TARGET)/include/pkcs11"; fi;      \
+#	if [ -d "$(BUILD_TARGET)/lib" ]; then rm -f -R "$(BUILD_TARGET)/lib"; fi;                            \
+#	Warn: under debug must specify -D_DEBUG_
+
+isContained() { [ -z "${2##*$1*}" ]; }
 usage() {
-	printf "%s\n"     "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"
+	printf "%s\n"     "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"
 	printf "%s\n"     "Nharu libraries installation tool"
 	printf "%s\n"     "Optional arguments:"
-	printf "%s\n"     "--prefix: libraries install directory. Default value:"
-	printf "\t%s\n"   "[nharu]/libs, where [nharu] refers to project diretory"
-	printf "%s\n"     "--openssl: OpenSSL installation directoy. If not supplied,"
-	printf "\t%s\n"   "it is sought from user's home directory"
-	printf "%s\n"     "--idn: GNU Libidn installation directoy. If not supplied,"
-	printf "\t%s\n"   "it is sought from user's home directory"
+	printf "%s\n"     "--target: target platform. Default value: linux"
+	printf "%s\n"     "--prefix: libraries root install directory for each target. Default value: $HOME/development/build"
+	printf "%s\n"     "--with-openssl: OpenSSL installation directoy. If not supplied, it is sought from $HOME directory"
+	printf "%s\n"     "--with-idn: GNU Libidn installation directoy. If not supplied, it is sought from $HOME directory"
 	printf "%s\n"     "--enable-FEATURE or --disable-FEATURE. Available features:"
 	printf "\t%s\n"   "java: if enabled, Java packages are built by Apache Ant;"
 	printf "\t%s\n"   "shared: if disabled JNI shared library is not built."
 	printf "%s\n"     "if --enable-java the following resources must be supplied:"
-	printf "\t%s\n"   "--jdk: JDK installation directoy. If not supplied and a"
-	printf "\t\t%s\n" "JAVA_HOME environment variable is not defined,"
-	printf "\t\t%s\n" "it is sought from user's home directory"
-	printf "\t%s\n"   "--ant: Apache Ant Java compiler utility. If not supplied,"
-	printf "\t\t%s\n" "it is sought from user's home directory"
-	printf "\t%s\n"   "--ant-contrib: Apache Ant utility. If not supplied,"
-	printf "\t\t%s\n" "it is sought from user's home directory"
-	printf "%s\n"     "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"
+	printf "\t%s\n"   "--with-jdk: JDK installation directoy or $JAVA_HOME environment variable"
+	printf "\t%s\n"   "--with-ant: Apache Ant Java compiler utility. If not supplied, it is sought from $HOME directory"
+	printf "\t%s\n"   "--with-ant-contrib: Apache Ant utility. If not supplied, it is sought from $HOME directory"
+	printf "%s\n"     "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *"
 }
 
 
@@ -35,7 +64,8 @@ while [ -h "$source" ]; do
 done
 CUR="$( cd -P "$( dirname "$source" )" && pwd )"
 PARENT=$(dirname $CUR)
-BUILD_TARGET="$PARENT/libs"
+TARGET="linux"
+BUILD_TARGET="$HOME/development/build"
 ENABLE_JAVA=
 ENABLE_SHARED="true"
 
@@ -56,6 +86,8 @@ do
 			;;
 	esac
 	case $key in
+		(--target) TARGET="$value"
+		;;
 		(--prefix) BUILD_TARGET="$value"
 		;;
 		(--enable-java) ENABLE_JAVA="true"
@@ -66,15 +98,15 @@ do
 		;;
 		(--disable-shared) ENABLE_SHARED=
 		;;
-		(--openssl) OPENSSL="$value"
+		(--with-openssl) OPENSSL="$value"
 		;;
-		(--idn) IDN="$value"
+		(--with-idn) IDN="$value"
 		;;
-		(--jdk) JDK="$value"
+		(--with-jdk) JDK="$value"
 		;;
-		(--ant) ANT_HOME="$value"
+		(--with-ant) ANT_HOME="$value"
 		;;
-		(--ant-contrib) ANT_CONTRIB="$value"
+		(--with-ant-contrib) ANT_CONTRIB="$value"
 		;;
 		(--help)
 			usage
@@ -88,14 +120,13 @@ do
 	esac
 	shift
 done
+BUILD_TARGET="$BUILD_TARGET/nharu/$TARGET"
 
 # DEPENDENCIES
 printf "%s\n" "Checking project dependencies..."
 if [ -z "$OPENSSL" ]; then
 	found=$(find $HOME -type d ! -perm -g+r,u+r,o+r -prune -o -name opensslconf.h -printf "%T@ %p\n" | sort -n | tail -1 | cut -f2- -d" ")
-	if [ -n "$found" ]; then
-		OPENSSL=$(dirname $(dirname $(dirname $found)))
-	fi
+	if [ -n "$found" ]; then OPENSSL=$(dirname $(dirname $(dirname $found))); fi
 fi
 if [ -f "$OPENSSL/include/openssl/opensslconf.h" -a -f "$OPENSSL/lib/libcrypto.a" ]; then
 	OPENSSL_INCLUDE="$OPENSSL/include"
@@ -108,9 +139,7 @@ printf "OpenSSL found at directory %s\n" "$OPENSSL"
 
 if [ -z "$IDN" ]; then
 	found=$(find $HOME -type d ! -perm -g+r,u+r,o+r -prune -o -name stringprep.h -printf "%T@ %p\n" | sort -n | tail -1 | cut -f2- -d" ")
-	if [ -n "$found" ]; then
-		IDN=$(dirname $(dirname $found))
-	fi
+	if [ -n "$found" ]; then IDN=$(dirname $(dirname $found)); fi
 fi
 if [ -f "$IDN/include/stringprep.h" -a -f "$IDN/lib/libidn.a" ]; then
 	IDN_INCLUDE="$IDN/include"
@@ -124,13 +153,10 @@ printf "GNU Libidn found at directory %s\n" "$IDN"
 # JNI DEPENDENCIES
 if [ -n "$ENABLE_SHARED" -o -n "$ENABLE_JAVA" ]; then
 	if [ -z "$JDK" ]; then
-		if [ -n "$JAVA_HOME" ]; then
-			JDK="$JAVA_HOME"
+		if [ -n "$JAVA_HOME" ]; then JDK="$JAVA_HOME";
 		else
 			java=$(find $HOME -type d ! -perm -g+r,u+r,o+r -prune -o -name jni.h -printf "%T@ %p\n" | sort -n | tail -1 | cut -f2- -d" ")
-			if [ -n "$java" ]; then
-				JDK=$(dirname $(dirname ($java)))
-			fi
+			if [ -n "$java" ]; then JDK=$(dirname $(dirname ($java))); fi
 		fi
 	fi
 	if [ -f "$JDK/include/jni.h" -a -f "$JDK/include/linux/jni_md.h" ]; then
@@ -153,12 +179,24 @@ fi
 
 # JAVA DEPENDENCIES
 if [ -n "$ENABLE_JAVA" ]; then
-# TODO
 	if [ -z "$ANT_HOME" ]; then
-printf ""
+		ant=$(find $HOME -type d ! -perm -g+r,u+r,o+r -prune -o -name runant.py -printf "%T@ %p\n" | sort -n | tail -1 | cut -f2- -d" ")
+		if [ -n "$ant" ]; then ANT_HOME=$(dirname $(dirname ($ant))); fi
 	fi
+	if [ ! -f "$ANT_HOME/bin/ant" ]; then
+		printf "%s\n" "Could not find Apache Ant installation directory"
+		exit 1
+	fi
+	printf "Apache Ant found at %s\n" "$ANT_HOME"
+	if [ -z "$ANT_CONTRIB" ]; then
+		ANT_CONTRIB=$(find $HOME -type d ! -perm -g+r,u+r,o+r -prune -o -name ant-contrib*.jar -printf "%T@ %p\n" | sort -n | tail -1 | cut -f2- -d" ")
+		if [ ! -f "$ANT_CONTRIB" ]; then
+			printf "%s\n" "Could not find Ant Contrib library"
+			exit 1
+		fi
+	fi
+	printf "Ant Contrib library found at %s\n" "$ANT_CONTRIB"
 fi
-
 
 # COMPILATION FLAGS
 if [ -z "$CC" ]; then
@@ -167,59 +205,35 @@ fi
 if [ -z "$AR" ]; then
 	AR="ar"
 fi
-if [ -z "$CXX" ]; then
-	CXX="gcc"
-fi
 if [ -z "$CFLAGS" ]; then
-	CFLAGS="-pedantic-errors -pedantic -Wall -ansi -pthread -Winline -Wunused-parameter"
+	CFLAGS="-pthread -pedantic-errors -pedantic -Wall -ansi -Winline -Wunused-parameter -O2"
 fi
 if [ -z "$ARFLAGS" ]; then
 	ARFLAGS="-r -s"
 fi
-if [ -z "$CXXFLAGS" ]; then
-	CXXFLAGS="-shared -shared-libgcc -Xlinker -z -Xlinker defs"
+if [ -z "$LDFLAGS" ]; then
+	LDFLAGS="-shared -shared-libgcc -Xlinker -z -Xlinker defs"
+	sys=$(uname -a | grep x86_64)
+	if [ -n "$sys" ]; then LDFLAGS="$LDFLAGS -fPIC"; fi
 fi
 CC="$CC"
 AR="$AR"
-CXX="$CXX"
 CFLAGS="$CFLAGS"
 ARFLAGS="$ARFLAGS"
-CXXFLAGS="$CXXFLAGS"
-sys=$(uname -a | grep x86_64)
-if [ -n "$sys" ]; then
-	CFLAGS="$CFLAGS -fPIC"
-	CXXFLAGS="$CXXFLAGS -fPIC"
-fi
+LDFLAGS="$LDFLAGS"
+if ! isContained "-shared" "$LDFLAGS" ; then LDFLAGS="$LDFLAGS -shared"; fi
+if ! isContained "-pthread" "$CFLAGS" ; then CFLAGS="$CFLAGS -pthread"; fi
 
 
 #CLEAN-UP
 printf "%s\n" "Nharu library old configuration clean-up..."
-if [ -f "$PARENT/src/Makefile" ]; then
-	make --directory="$PARENT/src" clean
-	rm -f "$PARENT/src/Makefile"
-fi
-if [ -f "$PARENT/jca/native/Makefile" ]; then
-	make --directory="$PARENT/jca/native" clean
-	rm -f "$PARENT/jca/native/Makefile"
-fi
-if [ -f "$PARENT/test/Makefile" ]; then
-	make --directory="$PARENT/test" clean
-	rm -f "$PARENT/test/Makefile"
-fi
-if [ -f "$PARENT/Makefile" ]; then
-	rm -f "$PARENT/Makefile"
-fi
+if [ -f "$PARENT/src/Makefile" ]; then make --directory="$PARENT/src" clean; fi
+if [ -f "$PARENT/jca/native/Makefile" ]; then make --directory="$PARENT/jca/native" clean; fi
+if [ -f "$PARENT/test/Makefile" ]; then make --directory="$PARENT/test" clean; fi
 
-
-printf "%s\n" "Makefiles generation..."
-# OUTPUT DIRECTORY CREATION
-mkdir -p "$BUILD_TARGET"
-if [ $? -ne 0 ]; then
-	printf "Could not create output %s directory\n" "$BUILD_TARGET"
-	exit $?
-fi
 
 # GENERATE NHARU ARCHIVE
+printf "%s\n" "Makefiles generation..."
 if [ ! -f "$CUR/static.in" ]; then
 	printf "Could not find file %s/static.in\n" "$CUR"
 	exit 1
@@ -229,9 +243,8 @@ printf "%s\n" "# Makefile of Nharu static library">>"$PARENT/src/Makefile"
 printf "%s\n" "# Copyleft (C) 2015 by The Crypthing Initiative">>"$PARENT/src/Makefile"
 printf "%s\n" "# Generated by configure. Do not edit">>"$PARENT/src/Makefile"
 printf "%s\n" "#">>"$PARENT/src/Makefile"
-printf "%s\n" "# Make optional parameters:">>"$PARENT/src/Makefile"
-printf "%s\n" "#	DEBUG=1">>"$PARENT/src/Makefile"
-printf "%s\n" "#	ALIGN=1">>"$PARENT/src/Makefile"
+printf "%s\n" "# For debugging set CFLAGS with -D_DEBUG_">>"$PARENT/src/Makefile"
+printf "%s\n" "# For structure alignment set CLFAGS with -D_ALIGN_">>"$PARENT/src/Makefile"
 printf "%s\n" "# # # # # # # # # # # # # # # # # # # # # # # #">>"$PARENT/src/Makefile"
 IFS=''
 while read -r line || [[ -n "$line" ]]; do
@@ -257,17 +270,15 @@ if [ -n "$ENABLE_SHARED" ]; then
 	printf "%s\n" "# Copyleft (C) 2015 by The Crypthing Initiative">>"$PARENT/jca/native/Makefile"
 	printf "%s\n" "# Generated by configure. Do not edit">>"$PARENT/jca/native/Makefile"
 	printf "%s\n" "#">>"$PARENT/jca/native/Makefile"
-	printf "%s\n" "# Make optional parameters:">>"$PARENT/jca/native/Makefile"
-	printf "%s\n" "#	DEBUG=1">>"$PARENT/jca/native/Makefile"
-	printf "%s\n" "#	ALIGN=1">>"$PARENT/jca/native/Makefile"
+	printf "%s\n" "# For debugging set CFLAGS with -D_DEBUG_">>"$PARENT/jca/native/Makefile"
+	printf "%s\n" "# For structure alignment set CLFAGS with -D_ALIGN_">>"$PARENT/jca/native/Makefile"
 	printf "%s\n" "# # # # # # # # # # # # # # # # # # # # # # # #">>"$PARENT/jca/native/Makefile"
 	IFS=''
 	while read -r line || [[ -n "$line" ]]; do
 		new="${line/\_CC_/$CC}"
-		new="${new/\_CXX_/$CXX}"
 		new="${new/\_BUILD_TARGET_/$BUILD_TARGET}"
 		new="${new/\_CFLAGS_/$CFLAGS}"
-		new="${new/\_CXXFLAGS_/$CXXFLAGS}"
+		new="${new/\_LDFLAGS_/$LDFLAGS}"
 		new="${new/\_OPENSSL_INCLUDE_/$OPENSSL_INCLUDE}"
 		new="${new/\_IDN_INCLUDE_/$IDN_INCLUDE}"
 		new="${new/\_JAVA_INCLUDE_/$JAVA_INCLUDE}"
@@ -290,14 +301,14 @@ printf "%s\n" "# Makefile Makefile Nharu static library test app">>"$PARENT/test
 printf "%s\n" "# Copyleft (C) 2015 by The Crypthing Initiative">>"$PARENT/test/Makefile"
 printf "%s\n" "# Generated by configure. Do not edit">>"$PARENT/test/Makefile"
 printf "%s\n" "#">>"$PARENT/test/Makefile"
-printf "%s\n" "# Make optional parameters:">>"$PARENT/test/Makefile"
-printf "%s\n" "#	DEBUG=1">>"$PARENT/test/Makefile"
+printf "%s\n" "# For debugging set CFLAGS with -D_DEBUG_">>"$PARENT/test/Makefile"
+printf "%s\n" "# For structure alignment set CLFAGS with -D_ALIGN_">>"$PARENT/test/Makefile"
 printf "%s\n" "# # # # # # # # # # # # # # # # # # # # # # # # # #">>"$PARENT/test/Makefile"
 IFS=''
 while read -r line || [[ -n "$line" ]]; do
 	new="${line/\_CC_/$CC}"
-	new="${new/\_CXX_/$CXX}"
 	new="${new/\_CFLAGS_/$CFLAGS}"
+	new="${new/\_LDFLAGS_/$LDFLAGS}"
 	new="${new/\_OPENSSL_INCLUDE_/$OPENSSL_INCLUDE}"
 	new="${new/\_IDN_INCLUDE_/$IDN_INCLUDE}"
 	new="${new/\_OPENSSL_LIB_/$OPENSSL_LIB}"
