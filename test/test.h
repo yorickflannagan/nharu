fc775fec8d0e8d486941c1f7182227b35ce55b20
Marco Gutierrez <yorick.flannagan@gmail.com>
2018-09-11 18:39:15 -0300

    Working day

--- test/test.h
+++ test/test.h
@@ -4,21 +4,22 @@
 #include "cms.h"
 
 int save_buffer(unsigned char*, size_t, char*);
-int test_encoder();
+int load_file(char*, unsigned char**, int*);
 
-int test_cadest();
+int test_encoder();
 int test_secret_sharing();
+int test_indefinite_length_form();
 int test_encrypt();
 int test_rsa();
-int test_cert();
-int test_pkibr();
-int test_crl();
+int test_certificate(char*, char*);
+int test_crl(char*, char*);
+
 int parse_openssl_cms_signed_data();
 int test_cms_signed_data();
 int test_cms_signed_data_with_pubkey();
 int test_openssl_cms_enveloped_data();
 int test_enveloped_data();
-int test_indefinite_length_form();
 int test_fake_enveloped_data();
+int test_cadest();
 
 #endif
